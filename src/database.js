3838d252b5c0dbc57693b0b2a2b1b0a98a44ffff
mrichte8 <max.richter@smail.th-koeln.de>
2020-06-10 17:12:31 +0200

    fix(dashboard): connect to wss in prod

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ const db = {};
 
 module.exports = {
   save: (msg, teamname) => {
+    console.log("save", msg, teamname);
     msg.created_at = Date.now();
     if (teamname in db) db[teamname].push(msg);
     else db[teamname] = [msg];
